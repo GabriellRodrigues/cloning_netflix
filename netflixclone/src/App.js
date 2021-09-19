00970ac5004d11ba54fd92a7ea60957d4c8495bb
GabriellRodrigues <gabriell_firminoo@hotmail.com>
2021-09-19 12:42:15 -0300

    feature: make recommended function

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import tmdb from './tmdb';
 import MovieRow from './components/MovieRow';
 import './App.css';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null)
 
   useEffect(() => {
     const loadAll = async () => {
@@ -19,6 +21,10 @@ export default () => {
   }, []);
   return (
     <div className="page">
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />  
+      }
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
