b69354fefa9855d090dd8848ab939ad2092e0b34
GabriellRodrigues <gabriell_firminoo@hotmail.com>
2021-09-18 21:59:46 -0300

    aading images and css

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,20 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import tmdb from './tmdb';
+import MovieRow from './components/MovieRow';
+import './App.css';
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const loadAll = async () => {
       // pegando a lista total
       let list = await tmdb.getHomelist();
+      setMovieList(list);
       console.log(list);
     }
 
     loadAll();
   }, []);
   return (
-    <div>
-      
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
-  )
+  );
 }
